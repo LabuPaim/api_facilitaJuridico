7d276d36202d0ac9a3ed8d27c4814f2332d9ec1a
victorSEMIT <victor.labussiere@salvador.ba.gov.br>
2024-03-06 11:23:07 -0300

    feat: cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import cors from "cors";
 import "./database/runMigrations";
 import { router } from "./useCases/router";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(3000, () => {
+app.listen(3001, () => {
 	console.log("HTTP Server running!");
 });
